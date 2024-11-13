01049e669e3f47e2fdaf807e12d32479994a53f5
chanksela <kakhachankseliani@gmail.com>
2024-11-13 16:34:36 +0400

    chore: * added window.ec configs to be consistent with routing * added home, settings and store page in vue router * injected ecwidscript for rendering ecwid store

---
+++ client/src/components/EcwidStore.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+	import { onMounted } from "vue";
+
+	const props = defineProps({
+		storeId: {
+			type: Number,
+			default: 101560752,
+		},
+	});
+
+	const injectEcwidScript = (storeId: number) => {
+		window.ec = window.ec || {};
+		window.ec.config = window.ec.config || {};
+		window.ec.config.baseUrl = "/store";
+		window.ec.config.storefrontUrls = window.ec.config.storefrontUrls || {};
+		window.ec.config.storefrontUrls.cleanUrls = true;
+
+		const ecwidScript = document.createElement("script");
+		ecwidScript.setAttribute("type", "text/javascript");
+		ecwidScript.setAttribute("charset", "utf-8");
+		ecwidScript.setAttribute("data-cfasync", "false");
+		ecwidScript.setAttribute(
+			"src",
+			`https://app.ecwid.com/script.js?${storeId}&data_platform=code&data`
+		);
+		ecwidScript.onload = () => injectEcwidProductBrowser(storeId);
+		document.head.appendChild(ecwidScript);
+	};
+	const injectEcwidProductBrowser = (storeId: number) => {
+		const ecwidBrowserScript = document.createElement("script");
+		ecwidBrowserScript.setAttribute("type", "text/javascript");
+		ecwidBrowserScript.setAttribute("charset", "utf-8");
+		ecwidBrowserScript.text = `xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-${storeId}");`;
+		document.head.appendChild(ecwidBrowserScript);
+	};
+	onMounted(() => {
+		injectEcwidScript(props.storeId);
+	});
+</script>
+<template>
+	<div :id="`my-store-${storeId}`"></div>
+</template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import EcwidStore from "../components/EcwidStore.vue";
+</script>
 
 <template>
 	<main>
 		<h1>Home View</h1>
+		<p>Landing Page</p>
 	</main>
 </template>
