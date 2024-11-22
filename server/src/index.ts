78926f343e1a44e26f614758baf3a47a849daa08
chanksela <kakhachankseliani@gmail.com>
2024-11-22 20:43:35 +0400

    feat: added feature to download products as csv

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,8 +9,23 @@ app.get("/", (req: Request, res: Response) => {
 	res.send("Hello World!");
 });
 app.post("/api/export", (req: Request, res: Response) => {
-	console.log("received body: ", req.body);
-	res.json({ message: "POST REQUEST WAS SUCCESS!", data: req.body });
+	function jsonToCsv(products: any) {
+		let csv = "";
+		for (let i = 0; i < products.length - 1; i++) {
+			const headers = Object.keys(products[0]);
+			csv += headers.join(",") + "\n";
+			products.forEach((obj: any) => {
+				const values = headers.map((header) => obj[header]);
+				csv += values.join(",") + "\n";
+			});
+		}
+
+		return csv;
+	}
+	const csvData = jsonToCsv(req.body.exportableProducts);
+	console.log(csvData);
+
+	res.attachment("products.csv").send(csvData);
 });
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
